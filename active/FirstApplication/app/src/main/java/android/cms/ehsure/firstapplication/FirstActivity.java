d9b3fd791fb662bdfd43190bfee3f6078c707b74
andresong <andresong@ehsure.com>
2018-02-02 18:56:16 +0800

    安卓学习工程提交

---
+++ active/FirstApplication/app/src/main/java/android/cms/ehsure/firstapplication/FirstActivity.java
@@ -0,0 +1,65 @@
+package android.cms.ehsure.firstapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FirstActivity extends AppCompatActivity {
+private Button btnTotast;
+private Button btnClose;
+private Button btnShowActivity;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+        btnTotast=(Button)findViewById(R.id.button1);
+        btnClose=(Button)findViewById(R.id.button2);
+        btnShowActivity=(Button)findViewById(R.id.btn_showActive);
+        btnShowActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(FirstActivity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        btnTotast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FirstActivity.this,"This is Toast",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.add_item:
+                Toast.makeText(this,"You Click Add",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this,"You Click Remove",Toast.LENGTH_LONG).show();
+                break;
+            default:
+        }
+        return true;
+    }
+}
